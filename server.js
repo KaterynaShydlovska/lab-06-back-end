022883bae908da2b35955992840c6cae06324240
katerynashydlovska <shydlovskayaka@gmail.com>
2019-11-05 12:46:13 -0800

    added weather

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+// let moment = require('moment');
+// moment().format();
+
 // Make my server
 const app = express();
 // Global vars
 const PORT = process.env.PORT || 3000;
 
-// app.get('/', (request, response) => {
-//   response.send('Hello you all');
-// });
-// app.use(cors());
 
 app.get('/location', (request, response) => {
   // send the users current location back to them
   const geoData = require('./data/geo.json');
   const city = request.query.data;
+  console.log('LOCATION END POINT REACHED')
   const locationData = new Location(city, geoData);
   response.send(locationData);
 });
@@ -32,6 +32,30 @@ function Location(city, geoData) {
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude = geoData.results[0].geometry.location.lng;
 }
+
+
+app.get('/weather', (request, response) => {
+  // send the users current location back to them
+  const forcast = require('./data/darksky.json');
+  console.log('WEATHER END POINT REACHED')
+  const weatherData = [];
+  for (let i = 0; i < forcast.daily.data.length; i++) {
+    let dataItem = forcast.daily.data[i]
+    weatherData.push(new Weather(dataItem.summary, dataItem.time));
+  }
+
+  response.send(weatherData);
+});
+
+function Weather(forcast, time) {
+  this.forcast = forcast;
+  this.time = new Date(parseInt(time*1000)).toGMTString();
+  // this.time = moment.utc(parseInt(time)).local()
+  // this.time = new Date(time);
+ 
+  
+}
+
 
 app.listen(PORT, () => {
   console.log(`listening on PORT ${PORT}`);
